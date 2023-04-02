fab55059f467ca7025e0747caecaba4aa6d0c830
Lavanya Natchiappan <lavee.natch@gmail.com>
2023-04-02 16:30:23 -0700

    updated employee table

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS work_db;
+CREATE DATABASE work_db;
+
+USE work_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE work_role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    FOREIGN KEY (role_id) 
+    REFERENCES work_role(id)
+    ON DELETE SET NULL,
+    manager_id INT
+    REFERENCES employee(id)
+);
